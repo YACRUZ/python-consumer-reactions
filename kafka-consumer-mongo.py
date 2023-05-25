dfb845fbf2054ebdda596da91150738cf52b1c8c
YACRUZ <111380939+YACRUZ@users.noreply.github.com>
2023-05-25 10:28:23 -0600

    Update kafka-consumer-mongo.py

--- kafka-consumer-mongo.py
+++ kafka-consumer-mongo.py
@@ -57,19 +57,20 @@ for msg in consumer:
        print("Could not insert into MongoDB")
     
     try:
-    agg_result= db.memes_info.aggregate(
-        [{
-            "group" :
-            { "_id" : "$name",
-             "n" : {"$sum" : 1}
-             }}
-        ])
-    db.memes_summary.delete_many({})
-    for i in agg_result:
-        print(i)
-        summary_id = db.memes_summary.insert_on(i)
-        print("Summary inserted with record ids", summary_id)
-
+        
+        agg_result= db.memes_info.aggregate(
+            [{
+                "group" :
+                { "_id" : "$name",
+                 "n" : {"$sum" : 1}
+                }}
+            ])
+        db.memes_summary.delete_many({})
+        
+        for i in agg_result:
+            print(i)
+            summary_id = db.memes_summary.insert_on(i)
+            print("Summary inserted with record ids", summary_id)
     except Exception as e:
         print(f'group by caught {type(e)}:')
         print(e)
